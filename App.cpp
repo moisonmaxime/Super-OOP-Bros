98d09e52c6059bc6602c7c9ba6f6cb840e1a43d0
xavier villa <xaviervilla@github.com>
2018-03-21 19:42:45 -0700

    remove default glutapp - add ctrl+c exit

--- App.cpp
+++ App.cpp
@@ -11,38 +11,12 @@ void App::draw() {
     // Clear the screen
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     
-    // Set background color to black
-    glClearColor(0.0, 0.0, 0.0, 1.0);
+    // Set background color to sky blue
+    glClearColor(0.26, 0.78, 0.95, 1.0);
     
     // Set up the transformations stack
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
-    
-    // Set Color
-    glColor3d(1.0, 1.0, 1.0);
-    
-    // Draw some points
-    glBegin(GL_POINTS);
-    
-    glVertex2f(0.5, 0.5);
-    glVertex2f(0.5, -0.5);
-    glVertex2f(-0.5, -0.5);
-    glVertex2f(-0.5, 0.5);
-    
-    glEnd();
-    
-    // Draw a yellow cross
-    glColor3d(1.0, 1.0, 0.0);
-    
-    glBegin(GL_LINES);
-    
-    glVertex2f(mx - 0.05f, my);
-    glVertex2f(mx + 0.05f, my);
-    
-    glVertex2f(mx, my - 0.05f);
-    glVertex2f(mx, my + 0.05f);
-    
-    glEnd();
     
     // We have been drawing everything to the back buffer
     // Swap the buffers to see the result of what we drew
@@ -69,8 +43,8 @@ void App::mouseDrag(float x, float y){
 }
 
 void App::keyPress(unsigned char key) {
-    if (key == 27){
-        // Exit the app when Esc key is pressed
+    if (key == 3 || key == 27){
+        // Exit the app when Esc key or ctr+c is pressed
         exit(0);
     }
 }
