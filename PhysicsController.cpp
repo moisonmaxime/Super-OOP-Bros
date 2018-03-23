d502aebfe34452b6c12395a0d3c587c9ad5cb93d
Maxime Moison <maximaxof@me.com>
2018-03-23 10:46:42 -0700

    Added

---
+++ PhysicsController.cpp
@@ -0,0 +1,9 @@
+//
+//  PhysicsController.cpp
+//  SuperOOPBros
+//
+//  Created by Maxime Moison on 3/23/18.
+//  Copyright © 2018 Maxime Moison. All rights reserved.
+//
+
+#include "PhysicsController.hpp"
---
+++ PhysicsController.hpp
@@ -0,0 +1,28 @@
+//
+//  PhysicsController.hpp
+//  SuperOOPBros
+//
+//  Created by Maxime Moison on 3/23/18.
+//  Copyright © 2018 Maxime Moison. All rights reserved.
+//
+
+#ifndef PhysicsController_hpp
+#define PhysicsController_hpp
+
+#include "MovingObject.hpp"
+
+#define DEFAULT_GRAVITY = 9.8
+#define DEFAULT_DRAG = 1
+
+class PhysicsController {
+    float gravity;
+    float drag;
+    void applyGravity(MovingObject* obj);
+    void applyDrag(MovingObject* obj);
+public:
+    PhysicsController();
+    PhysicsController(float drag, float gravity);
+    void applyforces(MovingObject* obj);
+};
+
+#endif /* PhysicsController_hpp */
