a85dc6dda77e5dfe90f658fd124dbe56744db208
Maxime Moison <maximaxof@me.com>
2018-05-07 21:42:25 -0700

    master merge into v2

--- Pipe.cpp
+++ Pipe.cpp
@@ -63,6 +63,12 @@ bool Pipe::collidesWith(Object* object) {
     return false;
 }
 
+bool Pipe::isBeingPassedBy(Object* object) {
+    float minX = x - WIDTH/2.0;
+    float maxX = x + WIDTH/2.0;
+    return (minX < object->getMinX()+0.02 && object->getMinX()+0.02 < maxX) || (minX < object->getMaxX()-0.02  && object->getMaxX()-0.02 < maxX);
+}
+
 void Pipe::calculateNextFrame(PhysicsController* pc) {
     x -= pc->getSpeed();
     if (x < -1.5) {
